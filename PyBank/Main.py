b5d7a6558c7fd8f85ea4e12de7e96472051a2c18
crisbusa <crisbusa92@github.com>
2020-07-04 14:37:03 -0500

    PyBank folder and file order. Bank py initial month counter

---
+++ PyBank/Main.py
@@ -0,0 +1,14 @@
+import os
+import csv
+# Set path for data file
+csvpath = os.path.join('Resources','pybank.csv')
+
+with open(csvpath,'r') as datafile:
+    #count number of rows/months starting at 0 and skipping the header
+    monthnum = 0
+    next(datafile, None)
+    for row in datafile:
+        monthnum = monthnum + 1
+    
+    
+print (monthnum)
