846d7e1b500989ec24b504ddb088960fe2fdef19
crisbusa <crisbusa92@github.com>
2020-07-04 15:47:21 -0500

    Generate List to calculate average monthly P&L

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -9,12 +9,17 @@ with open(csvpath,'r') as datafile:
     #Set Variables to start
     monthnum = 0
     pl = 0
+    pnls = []
     #Skip header row
     next(datafile, None)
     #Loop opps to get results
     for row in datafile:
+        #Find number of total months
         monthnum = monthnum + 1
+        #Find total P&L for all periods
         pl= pl + int(row[1])
+        #Greate new list to average
+        pnls.append(row)
 
     
 #print results to check    
