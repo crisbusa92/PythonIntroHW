5f95322803b899dc036b5586a7189d1ea72a81ce
crisbusa <crisbusa92@github.com>
2020-07-04 19:52:09 -0500

    Finished PyBank Code (not printing well yet)

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -13,22 +13,43 @@ with open(csvpath,'r') as datafile:
     monthnum = 0
     pl = 0
     pnls = []
+    month = []
+    greati = 0
+    greatd = 0
+    greatim = ''
+    greatdm = ''
+
     #Skip header row
     next(datafile, None)
+
     #Loop opps to get results
     for row in datafile:
+
         #Find number of total months
         monthnum = monthnum + 1
+
         #Find total P&L for all periods
         pl= pl + int(row[1])
+
         #Greate new list to average
         pnls.append(int(row[1]))
 
-
-#print results to check    
-print (monthnum)
-print (pl)
-print (average(pnls))
+        #Generate month list
+        month.append(row[0])
 
 
-
+#Find Greatest Increase and Greatest Decrease in profits
+greati = max(pnls)
+greatd = min(pnls)
+#Find month for GreatestIncrease and GreatestDecrease        
+greatim = month[pnls.index(greati)]
+greatdm = month[pnls.index(greatd)]
+#Print Results
+print(
+    f'Financial Analysis\n'
+    '----------------------\n'  
+    'Total Months {monthnum}\n' 
+    'Total: ${pl}\n'
+    'Greatest increase in profits: {greatim} ({greati}) \n'
+    'Greatest decrease in profits: {greatdm} ({greatd})'
+    )
